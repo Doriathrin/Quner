796dd70fd2065762bd3c67e1bf866995e209316a
liwenqiang <1282420341@qq.com>
2020-02-27 16:09:56 +0800

    this is icons project

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <HomeHeader></HomeHeader>
     <HomeSwiper></HomeSwiper>
+    <HomeIcons></HomeIcons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/HomeHeader'
 import HomeSwiper from './components/HomeSwiper'
+import HomeIcons from './components/HomeIcons'
 export default {
   name: 'Home',
   components:{
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   },
   data() { 
     return {
---
+++ src/pages/home/components/HomeIcons.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home-icons">
+    <div class='icon'>
+      <div class="icon-img">
+        <img class='icon-img-content' src="" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data() { 
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+ }
+</script>
+
+<style lang="stylus" scoped>
+.home-icons
+  overflow hidden
+  height:0
+  padding-bottom:50%
+  background:green
+  .icon
+    float:left
+    width:25%
+    padding-bottom:25%
+    background:red
+</style>
