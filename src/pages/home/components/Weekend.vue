12556d6e026c9061000d693286e3e9f5fe1994fd
liwenqiang <1282420341@qq.com>
2020-03-01 15:11:18 +0800

    project commend weekend 完成

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="weekend">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weekend',
+  data() { 
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+ }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
