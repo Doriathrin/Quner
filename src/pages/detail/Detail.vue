8f9cec982e563deb913eee7b9c4c301207f64255
Lionheart <1282420341@qq.com>
2020-03-09 17:01:26 +0800

    Slide show hide project

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,30 +1,60 @@
 <template>
   <div class="detail">
     <DetailBanner></DetailBanner>
+    <DetailHeader></DetailHeader>
+    <div class="content">
+      <DetailList :list='list'></DetailList>
+    </div>
   </div>
 </template>
 
 <script>
+import DetailList from './components/List'
+import DetailHeader from './components/Header'
 import DetailBanner from './components/Banner'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader,
+    DetailList
   },
-  data() { 
+  data () {
     return {
-
+      list: [{
+        "title": "成人票",
+        "children": [{
+          "title": "成人三馆联票",
+          "children": [{
+            "title": "成人三馆联票 - 某一连锁店销售",
+            "children": [
+              {
+                "title": "成人四馆联票 - 某第四级连锁店销售"
+              }
+            ]
+          }]
+        }, {
+          "title": "成人五馆联票"
+        }]
+      }, {
+        "title": "学生票"
+      }, {
+        "title": "儿童票"
+      }, {
+        "title": "特惠票"
+      }]
     }
   },
   methods: {
 
   },
-  mounted() {
+  mounted () {
 
   },
- }
+}
 </script>
 
 <style lang="stylus" scoped>
-
+.content
+  height 50rem
 </style>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="">
+    <div class="item" v-for='(item,key) of list' :key='key'> 
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div class="item-chilren" v-if='item.children'>
+        <!-- 组件内部自己调用自身 -->
+        <DetailList :list='item.children'></DetailList>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props:{
+    list:Array
+  },
+  data() { 
+    return {
+      
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+ }
+</script>
+
+<style lang="stylus" scoped>
+.item-title-icon
+  display: inline-block;
+  position: absolute;
+  width: .36rem;
+  height: .36rem;
+  top: .26rem;
+  left: .1rem;
+  background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+  margin-right: .1rem;
+  background-size: .4rem 3rem;
+.item-title
+  line-height:0.8rem;
+  font-size:0.32rem;
+  padding:0 0.5rem;
+</style>
